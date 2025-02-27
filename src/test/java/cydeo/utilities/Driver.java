6978ddbdfc38b4d11eadd7ab2dd77b40c5545691
jasont <jason101real@gmail.com>
2025-02-26 19:43:58 -0600

    updated chrome driver

--- src/test/java/cydeo/utilities/Driver.java
+++ src/test/java/cydeo/utilities/Driver.java
@@ -57,6 +57,10 @@ public class Driver {
                         URL url = new URL("http://"+ gridAddress + ":4444/wd/hub");
                         ChromeOptions chromeOptions = new ChromeOptions();
                         chromeOptions.addArguments("--start-maximized");
+                        chromeOptions.setBinary("/usr/lib/chromium-browser/chromium-browser"); // Default Chromium path on Ubuntu
+                        chromeOptions.addArguments("--headless"); // Enable headless mode for Jenkins
+                        chromeOptions.addArguments("--no-sandbox"); // Bypass sandboxing issues in CI
+                        chromeOptions.addArguments("--disable-dev-shm-usage"); // Avoid shared memory issues
                         driverPool.set(new RemoteWebDriver(url, chromeOptions));
                         //driverPool.set(new RemoteWebDriver(new URL("http://0.0.0.0:4444/wd/hub"),desiredCapabilities));
 
